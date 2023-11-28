e04cde888c61c9b7a7b7db50afdc464a40c5c521
Jafar Jabbarli <jafar.jabbarli@ram.lan>
2023-11-28 17:05:01 +0400

    initial commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import Container from "@/app/components/Container";
+
+const Navbar = () => {
+    return (
+        <div className="py-4 border-b-[1px]">
+            <Container>
+                fucking shit
+            </Container>
+        </div>
+    )
+}
+export default Navbar;
